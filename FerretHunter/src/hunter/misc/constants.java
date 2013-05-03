a5570db1391f52dcf954dd8f657473f9fdc9d4d6
Eluma <guardian_cm@hotmail.com>
2013-05-04 01:37:14 +0800

    rm everything to re-add

--- FerretHunter/src/hunter/misc/constants.java
+++
@@ -1,9 +0,0 @@
-package hunter.misc;
-
-public class constants 
-{
-	public static final int [] boxTrap = {19191,19192};//success,failure
-	public static final int ferretID = 10092;
-	public static final int boxTrapID = 10008;
-	public static final int placedBoxTrapID = 19187;
-}
